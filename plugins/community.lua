7caba55f0da54459118042222e7a497b7da248f8
Muhamad Farhan <muhamadfarhan91@gmail.com>
2023-07-17 08:07:19 +0700

    feat(community): add more plugins from astrocummunity

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,50 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  disabled = true,
   -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- available plugins can be found at
+  -- https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- language pack
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+
+  -- note taking
+  { import = "astrocommunity.note-taking.neorg" },
+
+  -- bars and lines
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+
+  -- indent
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+
+  -- code runner
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.code-runner.sniprun" },
+
+  -- project
+  { import = "astrocommunity.project.project-nvim" },
+
+  -- diagnostics
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  -- git
+  { import = "astrocommunity.git.neogit" },
+
+  -- motion
+  -- { import = "astrocommunity.motion.mini-basics" },
+  { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.motion.flash-nvim" },
 }
